ada70dde6d68dba6ad94e7882599ee02eff40b36
iftachyakar <fatach>
2024-12-03 21:50:05 +0200

    Added some fixes for pymatching simulation method (it now fails much less) - Added simulation+plot of constant qubit count with varying error. - Added option to give pymatching a "noiseless reference circuit" which should prevent it from "knowing" which errors happened. However it doesn't work yet so not using it...

--- stim_utils.py
+++ stim_utils.py
@@ -3,13 +3,19 @@ import numpy as np
 import pymatching
 
 # Counting errors using pymatching to create a decoder
-def count_logical_errors(circuit: stim.Circuit, num_shots: int) -> int:
+def count_logical_errors(circuit: stim.Circuit, num_shots: int, noiseless_reference_circuit: stim.Circuit=None) -> int:
     # Sample the circuit.
     sampler = circuit.compile_detector_sampler()
     detection_events, observable_flips = sampler.sample(num_shots, separate_observables=True)
 
-    # Configure a decoder using the circuit.
-    detector_error_model = circuit.detector_error_model(decompose_errors=True)
+    if (noiseless_reference_circuit):
+        # Configure a decoder using a reference noiseless circuit.
+        # this prevents pymatching from knowing which errors happened in case we simulate them explicitly 
+        # https://quantumcomputing.stackexchange.com/questions/32560/using-deterministic-errors-in-stim-detector-circuit-and-decoding-with-pymatching
+        detector_error_model = noiseless_reference_circuit.detector_error_model(decompose_errors=True)
+    else:
+        # Configure a decoder using the circuit.
+        detector_error_model = circuit.detector_error_model(decompose_errors=True)
     matcher = pymatching.Matching.from_detector_error_model(detector_error_model)
 
     # Run the decoder.
@@ -23,3 +29,4 @@ def count_logical_errors(circuit: stim.Circuit, num_shots: int) -> int:
         if not np.array_equal(actual_for_shot, predicted_for_shot):
             num_errors += 1
     return num_errors
+#%%
